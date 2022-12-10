190ccc5b6b47ebecad32353a980a5bc7c0720938
Alok Tamrakar <aloktamrakar2@gmail.com>
2022-12-10 16:16:27 +0530

    initial commit

---
+++ src/Pages/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Table from '../Component/Table'
+
+function Homepage() {
+  return (
+    <div>
+      <Table/>
+
+      
+    </div>
+  )
+}
+
+export default Homepage
