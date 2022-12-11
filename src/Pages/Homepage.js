80ede8a2609aa7764216e1239343f61d15c52282
PRANKUR PANDEY <pprankur@gmail.com>
2022-12-11 11:03:33 +0530

    added data

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,14 +1,31 @@
-import React from 'react'
-import Table from '../Component/Table'
+import React, { useEffect, useState } from "react";
+import Tables from "../Component/Tables";
 
 function Homepage() {
+  const [rows, setRows] = useState([]);
+  console.log("🚀 ~ file: Homepage.js:6 ~ Homepage ~ rows", rows);
+
+  const url = "https://reqres.in/api/users";
+  useEffect(() => {
+    async function getData() {
+      try {
+        let parsedObj = await fetch(url)
+          .then((response) => response.json())
+          .then((data) => setRows(data.data))
+          .catch((error) => console.log(error));
+        return parsedObj;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getData();
+  }, []);
+
   return (
     <div>
-      <Table/>
-
-      
+      <Tables rows={rows} />
     </div>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
